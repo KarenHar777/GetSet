e267f8159561d43728f68fd3c414dae6beb6a8cd
KarenHarutyunyan <karenvardanovich@gmail.com>
2022-12-23 21:58:09 +0400

    Add project files.

---
+++ InchVorBan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class InchVorBan
+    {
+
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; } = 8;
+
+        private int _filds;
+
+        public int p_write
+        {
+            set { _filds = value; }
+        }
+        public int p
+        {
+
+            get { return _filds; }
+            set {
+
+                if (value > 0)
+                {
+                    _filds = value;
+                }
+                else
+                {
+                    
+                    throw new NullReferenceException("Name is Empty");
+                }
+                 }
+        }
+        public InchVorBan()
+        {
+
+        }
+        public InchVorBan(int a, int b,int c)
+        {
+            MyProperty = a;
+            MyProperty2 = b;
+            _filds = c;
+
+            Console.WriteLine("Arjeqner  "+a+" "+b+" "+ c);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InchVorBan obj1 = new InchVorBan();
+            Console.WriteLine(obj1.MyProperty2);
+            
+            InchVorBan obj2 = new InchVorBan(6,5,6);
+
+            //obj2.p_write = 111;
+            obj1.p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(obj1.p);
+            Console.ReadKey();
+
+        }
+    }
+}
